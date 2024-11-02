0ae2c0691e7558ad210fc60e4b00c969524036a7
coryallegory <corymetcalfe@gmail.com>
2024-11-02 05:41:31 +0000

    Deploying to gh-pages from  @ 9f28302337751cedb38e9bfd4009187606b625f9 🚀

--- js/xp.js
+++ js/xp.js
@@ -12,8 +12,8 @@ function isToday(someDate) {
 var dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
 var dateNoYearOptions = { month: 'long', day: 'numeric' };
 var today = new Date();
-var seasonStart = new Date("08/16/2024");
-var seasonEnd = new Date("11/02/2024");
+var seasonStart = new Date("11/02/2024");
+var seasonEnd = new Date("12/01/2024");
 var daysRemaining = Math.ceil( (seasonEnd.getTime() - today.getTime()) / (1000 * 3600 * 24) );
 var daysTotal = Math.ceil( (seasonEnd.getTime() - seasonStart.getTime()) / (1000 * 3600 * 24) );
 var xp100PerDay = totalPerLevel[100]/daysTotal;
